b87d7ef9ec3f46099bf7e4018bd0ed905d9d74ca
Kairu <148830902+IKairuu@users.noreply.github.com>
2026-05-18 21:10:19 +0800

    Leetcode longest substring without repetition
    Implement a method to find the length of the longest substring without repeating characters.

---
+++ longest_substring_3.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s): # O(n^2) can still be improve using dictionary
+        """
+        :type s: str
+        :rtype: int
+        """
+        subs = []
+        highest = 0
+        for x in range(len(s)):
+            current = []
+            if s[x:x+95] == s[x+95:x+(95+95)]:
+                continue
+            else:
+                for y in s[x:]:
+                    if y in current:
+                        if len(current) > highest:
+                            highest = len(current)
+                        subs.append(current)
+                        current = []
+                               
+                    current.append(y)
+            if highest < len(current):
+                highest = len(current)
+            subs.append(current)
+        return highest
+            
+
+
+        
